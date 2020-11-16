4d2d3d1ed2225b19955617127fc829d6b29cf0fb
SreeCharanManne <51262836+SreeCharanManne@users.noreply.github.com>
2020-11-16 14:08:05 +0530

    Update GradingDriver.java

--- GradingDriver.java
+++ GradingDriver.java
@@ -10,16 +10,8 @@ import org.apache.hadoop.util.*;
 
 
 public class GradingDriver {
-    //public static class GradingMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable>
     public static class GradingMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable>{
         private static int mapNumber = 0;
-   
-        // protected void setup(Context context) {
-        //     this.mapNumber++;
-        //     System.out.println("Map" + String.valueOf(this.mapNumber) + ": Start");
-        // }
-    
-    
         public void map(LongWritable longWritable, Text text, OutputCollector<Text,IntWritable>output,Reporter reporter) throws IOException{
             String line = text.toString();
             String[] keyValue = line.split("\t");
@@ -31,17 +23,8 @@ public class GradingDriver {
     
       
     }
-    
-    //public static class GradingReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> 
     public static class GradingReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable>{
         private static int reduceNumber = 0;
-     
-        // protected void setup(Context context) {
-        //     this.reduceNumber++;
-        //     System.out.println("Reduce" + String.valueOf(this.reduceNumber) + ": Start");
-        // }
-    
-     
         public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text,IntWritable>output,Reporter reporter) throws IOException{
             String studentId = key.toString();
             int totalScore = 0;
@@ -50,21 +33,11 @@ public class GradingDriver {
                 totalScore += score;
             }
             output.collect(new Text(studentId), new IntWritable(totalScore));
-            // or you can use the following instead
-            //context.write(key, new IntWritable(totalScore));
         }
 
     }
     
     public static void main(String[] args) throws Exception {
-        // Path inputDirPath = new Path("src/main/resources/input/grading/");
-        // Path outputDirPath = new Path("src/main/resources/output/grading/");
-        // Configuration conf = new Configuration();
-        // conf.set("fs.defaultFS", "file:/");
-        // conf.set("mapreduce.framework.name", "local");
-        // FileSystem fs = FileSystem.getLocal(conf);
-        // fs.delete(outputDirPath, true);
-        // fs.setWriteChecksum(false);
         JobConf job = new JobConf(GradingDriver.class);
         job.setJobName("Grading_Driver");
         job.setOutputKeyClass(Text.class);
@@ -74,8 +47,6 @@ public class GradingDriver {
         job.setReducerClass(GradingReducer.class);
         job.setInputFormat(TextInputFormat.class);
         job.setOutputFormat(TextOutputFormat.class);
-        // Output Key and Value: same to E and F in GradingReducer
-       
         FileInputFormat.setInputPaths(job,new Path(args[0]));
         FileOutputFormat.setOutputPath(job,new Path(args[1]));
         JobClient.runJob(job);
